7a35cfcb6a4ca45445b036dedcf358327b4c85d8
alexgabriel8 <alexgabriels8@outlook.com>
2023-12-20 14:00:16 -0300

    Game state equal to "Implementando a lógica dos duelos"

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -93,6 +93,32 @@ async function setCardsField(cardId) {
     await drawButton(duelResults);
 }
 
+async function updateScore() {
+    state.score.scoreBox.innerText = `Win: ${state.score.playerScore} | Lose: ${state.score.computerScore}`
+}
+
+async function drawButton(text) {
+    state.actions.button.innerText = text;
+    state.actions.button.style.display = "block";
+}
+
+async function checkDuelResults(playerCardId, computerCardId) {
+    let duelResults = "Empate";
+    let playerCard = cardData[playerCardId];
+
+    if(playerCard.winOf.includes(computerCardId)) {
+        duelResults = "Ganhou";
+        state.score.playerScore++
+    }
+
+    if(playerCard.loseOf.includes(computerCardId)) {
+        duelResults = "Perdeu";
+        state.score.computerScore++
+    }
+
+    return duelResults
+}
+
 async function removeAllCardsImages() {
     let {computerBox, playerBox} = state.playerSides;
     let imgElements = computerBox.querySelectorAll("img")
